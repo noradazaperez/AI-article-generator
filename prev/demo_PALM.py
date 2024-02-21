c2a8571ab442d6ba959efe75ac1fca720db16d2f
noraborabora <noraborabora@github.com>
2024-02-21 09:45:43 +0100

    unstage

--- prev/demo_PALM.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as palm
-
-API_KEY = 'AIzaSyDJLl-f-jRe28WVcN0NkI7n2e5BF243ViM'
-palm.configure(api_key=API_KEY)
-
-model_list = [_ for _ in palm.list_models()]
-for model in model_list:
-    print(model.name)
